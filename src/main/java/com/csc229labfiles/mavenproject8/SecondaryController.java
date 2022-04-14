9378c1fc0b6f8fd15978e1ad8482edaea49f2e5c
AdamSolomon8675 <soloa14@farmingdale.edu>
2022-04-13 21:57:14 -0400

    added Gui

---
+++ src/main/java/com/csc229labfiles/mavenproject8/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.csc229labfiles.mavenproject8;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}

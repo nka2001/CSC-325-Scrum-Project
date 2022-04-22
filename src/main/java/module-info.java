9a6c66606397650d2a41d934db7dc7050b463dc9
sulem <sulem@MeowbookAir.home>
2022-04-22 18:14:20 -0400

    Removed module-info

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.csc325covidtracker {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.csc325covidtracker to javafx.fxml;
-    exports com.mycompany.csc325covidtracker;
-}
